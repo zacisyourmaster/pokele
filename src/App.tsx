20bbd9dcad3b3bf89f2eb5ee50b89b44a0a9d80b
zacisyourmaster <zac15590@gmail.com>
2026-03-30 17:34:39 -0700

    added vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,19 @@ import Header from "./components/Header";
 import Game from "./components/Game";
 import Footer from "./components/Footer";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react";
 // import img_src from "./assets/whos_that_pokemon.png"
 export default function App() {
   return (
     <div
-      className="
-        flex flex-col items-center
-        w-full md:max-w-5xl overflow-x-hidden
-        mx-auto relative space-y-8 
-      bg-neutral-800/30 backdrop-blur-md 
-        shadow-lg p-4 sm:p-6 md:p-8"
+      className="relative mx-auto flex w-full flex-col items-center space-y-8 overflow-x-hidden bg-neutral-800/30 p-4 shadow-lg backdrop-blur-md sm:p-6 md:max-w-5xl md:p-8"
       id="app"
     >
       <Header />
       <Game />
       <Footer />
       <Toaster position="top-center" />
+      <Analytics />
     </div>
   );
 }
